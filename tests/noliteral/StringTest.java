1b1525d5722c68e9cf5d15fa80e3b9bfad6a983e
Martin Kellogg <kelloggm@cs.washington.edu>
2020-02-21 16:51:05 -0800

    add String stub file + tests

--- tests/noliteral/StringTest.java
+++ tests/noliteral/StringTest.java
@@ -1,8 +1,15 @@
 // An example of code that was issuing a surprising warning in the
 // NSI example.
 
+import org.checkerframework.checker.noliteral.qual.*;
+
 class StringTest {
     void test(String info) {
         String[] splitInfo = info.split("/");
+    }
+
+    void splitConstant() {
+        // :: error: assignment.type.incompatible
+        @NonConstant String[] splits = "let's split this string".split(" ");
     }
 }
