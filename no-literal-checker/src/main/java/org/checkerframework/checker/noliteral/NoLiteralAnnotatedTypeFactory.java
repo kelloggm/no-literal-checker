e5d2f9a7541b1a2581197770a08f010cc28a2a06
Martin Kellogg <kelloggm@cs.washington.edu>
2020-03-12 11:57:17 -0700

    fix defaulting for array component types in bytecode

--- no-literal-checker/src/main/java/org/checkerframework/checker/noliteral/NoLiteralAnnotatedTypeFactory.java
+++ no-literal-checker/src/main/java/org/checkerframework/checker/noliteral/NoLiteralAnnotatedTypeFactory.java
@@ -1,14 +1,20 @@
 package org.checkerframework.checker.noliteral;
 
 import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.util.Elements;
 import org.checkerframework.checker.noliteral.qual.MaybeDerivedFromConstant;
 import org.checkerframework.checker.noliteral.qual.NonConstant;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.qual.TypeUseLocation;
+import org.checkerframework.framework.type.AnnotatedTypeFactory;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
+import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedArrayType;
 import org.checkerframework.framework.util.defaults.QualifierDefaults;
 import org.checkerframework.javacutil.AnnotationBuilder;
+import org.checkerframework.javacutil.ElementUtils;
 
 /** The type factory for the no literal checker. */
 public class NoLiteralAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
@@ -51,7 +57,7 @@ public class NoLiteralAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
 
   /**
    * An extension to QualifierDefaults that does nothing instead of adding the standard unchecked
-   * qualifier defaults, since they override the defaults added above for PARAMETER and LOWER_BOUND.
+   * qualifier defaults, since they override the defaults added above for PARAMETER.
    *
    * <p>TODO: is that a bug in QualifierDefaults? It looks like it's not supposed to override
    * anything...
@@ -66,6 +72,63 @@ public class NoLiteralAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
     @Override
     public void addUncheckedStandardDefaults() {
       // do nothing
+    }
+
+    /**
+     * Replace the normal default applier element with one that also applies the defaults to the
+     * component types of arrays. For the no-literal checker, it makes a lot more sense to talk
+     * about "a non-constant array of string constants" than it does to say "a constant array of
+     * non-constant strings" - the latter doesn't even make sense. So to do optimistic defaulting in
+     * bytecode correctly, the defaults need to be applied all the way down through an array.
+     */
+    @Override
+    protected DefaultApplierElement createDefaultApplierElement(
+        AnnotatedTypeFactory atypeFactory,
+        Element annotationScope,
+        AnnotatedTypeMirror type,
+        boolean applyToTypeVar) {
+      // Must check because the defaults for source code must not change to avoid interfering with
+      // CLIMB-to-top
+      // local type inference.
+      if (ElementUtils.isElementFromByteCode(annotationScope)) {
+        return new NoLiteralDefaultApplierElement(
+            atypeFactory, annotationScope, type, applyToTypeVar);
+      } else {
+        return super.createDefaultApplierElement(
+            atypeFactory, annotationScope, type, applyToTypeVar);
+      }
+    }
+
+    /**
+     * Modification of the DefaultApplierElement to also apply defaults to array component types.
+     *
+     * <p>See summary comment above for reasoning.
+     */
+    private class NoLiteralDefaultApplierElement extends DefaultApplierElement {
+
+      public NoLiteralDefaultApplierElement(
+          AnnotatedTypeFactory atypeFactory,
+          Element annotationScope,
+          AnnotatedTypeMirror type,
+          boolean applyToTypeVar) {
+        super(atypeFactory, annotationScope, type, applyToTypeVar);
+      }
+
+      /**
+       * Add the qualifier to the type if it does not already have an annotation in the same
+       * hierarchy as qual.
+       *
+       * @param type type to add qual
+       * @param qual annotation to add
+       */
+      @Override
+      protected void addAnnotation(AnnotatedTypeMirror type, AnnotationMirror qual) {
+        super.addAnnotation(type, qual);
+        if (type.getKind() == TypeKind.ARRAY) {
+          AnnotatedArrayType asArrayType = (AnnotatedArrayType) type;
+          addAnnotation(asArrayType.getComponentType(), qual);
+        }
+      }
     }
   }
 
