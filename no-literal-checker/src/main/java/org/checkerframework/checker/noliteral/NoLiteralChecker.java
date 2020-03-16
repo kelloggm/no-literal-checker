2ef4a3124b6d7d073d21712d418f501cfec61f96
Martin Kellogg <kelloggm@cs.washington.edu>
2020-03-16 12:39:05 -0700

    formatting

--- no-literal-checker/src/main/java/org/checkerframework/checker/noliteral/NoLiteralChecker.java
+++ no-literal-checker/src/main/java/org/checkerframework/checker/noliteral/NoLiteralChecker.java
@@ -1,7 +1,6 @@
 package org.checkerframework.checker.noliteral;
 
 import org.checkerframework.common.basetype.BaseTypeChecker;
-import org.checkerframework.framework.qual.StubFiles;
 
 /** The NoLiteral Checker enforces that literals don't go where they don't belong */
 public class NoLiteralChecker extends BaseTypeChecker {
