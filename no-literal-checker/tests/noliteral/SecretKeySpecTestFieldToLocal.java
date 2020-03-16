a99f405b346c3ec241cceb14b6dee7e575c6bb1a
Martin Kellogg <kelloggm@cs.washington.edu>
2020-03-16 14:36:19 -0700

    commit test case that shows a bit of weird behavior wrt where errors are issued, but also demos that there's no fp on correct codE

---
+++ no-literal-checker/tests/noliteral/SecretKeySpecTestFieldToLocal.java
@@ -0,0 +1,52 @@
+// An example of an error that's legitimate, but is issued in
+// the wrong place. For now, I'm okay with this error showing up
+// in the wrong place, because on correct code it isn't issued -
+// see the second part of the example. Fixing this would require
+// the CF to support local inference of array component types.
+//
+// This example assumes that WPI has been run already.
+
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+
+import org.checkerframework.checker.noliteral.qual.MaybeDerivedFromConstant;
+
+public class SecretKeySpecTestFieldToLocal {
+
+    private static final @MaybeDerivedFromConstant String KEY = "kj34PXF65ze70uFG";
+
+    public SecretKeySpec getKeySpecBad() {
+        byte[] theBytes = new byte[0];
+
+        try {
+            // :: error: assignment.type.incompatible
+            theBytes = KEY.getBytes("utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        // it would be better if the error was issued here
+        return new SecretKeySpec(theBytes, "AES");
+    }
+
+    private String secretKey;
+
+    public SecretKeySpec getKeySpecOk() {
+        if (secretKey != null) {
+            byte[] theBytes = new byte[0];
+            try {
+                //
+                theBytes = secretKey.getBytes("utf-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            return new SecretKeySpec(theBytes, "AES");
+        } else {
+            return null;
+        }
+    }
+
+    public void setSecretKey(String theKey) {
+        secretKey = theKey;
+    }
+}
