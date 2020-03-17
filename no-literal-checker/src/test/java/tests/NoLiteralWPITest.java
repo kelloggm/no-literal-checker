5a6778c484d3699c58a457ef9e8449855e5dd203
Martin Kellogg <kelloggm@cs.washington.edu>
2020-03-16 18:44:30 -0700

    specify what the test does

--- no-literal-checker/src/test/java/tests/NoLiteralWPITest.java
+++ no-literal-checker/src/test/java/tests/NoLiteralWPITest.java
@@ -6,7 +6,10 @@ import org.checkerframework.checker.noliteral.NoLiteralChecker;
 import org.checkerframework.framework.test.CheckerFrameworkPerDirectoryTest;
 import org.junit.runners.Parameterized.Parameters;
 
-/** Test runner that runs the No Literal Checker with WPI. */
+/**
+ * Test runner that runs the No Literal Checker with WPI. This just checks that there are no errors
+ * when running with WPI, not that the results of WPI are correct.
+ */
 public class NoLiteralWPITest extends CheckerFrameworkPerDirectoryTest {
   public NoLiteralWPITest(List<File> testFiles) {
     super(
