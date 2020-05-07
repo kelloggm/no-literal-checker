2f080c37507a0ea6c053adf62cf4cc01a8690124
Martin Kellogg <kelloggm@cs.washington.edu>
2020-05-07 08:39:27 -0700

    handle javax.swing.JOptionPane showInputDialog

--- no-literal-checker/tests/noliteral/Inputs.java
+++ no-literal-checker/tests/noliteral/Inputs.java
@@ -2,6 +2,8 @@
 
 import org.checkerframework.checker.noliteral.qual.*;
 
+import javax.swing.JOptionPane;
+
 class Inputs {
     void testSystem() {
         @NonConstant String variable = System.getenv("CHECKERFRAMEWORK");
@@ -9,5 +11,18 @@ class Inputs {
         @NonConstant String property2 = System.getProperty("java.home", property);
         // :: error: assignment.type.incompatible
         @NonConstant String property3 = System.getProperty("java.home", "x");
+    }
+
+    // these examples are from https://docs.oracle.com/javase/8/docs/api/javax/swing/JOptionPane.html
+    void testSwing() {
+        @NonConstant String inputValue = JOptionPane.showInputDialog("Please input a value");
+
+        Object[] possibleValues = { "First", "Second", "Third" };
+
+        // :: error: assignment.type.incompatible
+        @NonConstant Object selectedValue = JOptionPane.showInputDialog(null,
+                "Choose one", "Input",
+                JOptionPane.INFORMATION_MESSAGE, null,
+                possibleValues, possibleValues[0]);
     }
 }
